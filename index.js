293914fb26aeb416f9d61f8c4d2f053541ad70ed
Nwose Lotanna <lotanna.nwose@meltwater.org>
2020-03-24 12:36:46 +0000

    increased the random number to 20

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log(data);
-	var random = Math.floor(Math.random() * 15);
+	var random = Math.floor(Math.random() * 20);
 	 document.getElementById('litter').innerHTML = data[random].suggestion;
 	 document.getElementById('heading1').innerHTML = data[random].heading;
 	 document.getElementById('paragraph1').innerHTML = data[random].paragraph; 
